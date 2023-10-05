776c6b34a84a535056325cd63024e8a99c25a7d0
Joel Niebergal <SESA523174@se.com>
2023-10-04 22:03:12 -0700

    Added increment and decrement buttons.

--- ServoKicker.ino
+++ ServoKicker.ino
@@ -17,21 +17,32 @@ and not sink your lines while fighting a fish. The led on the button is lit, let
 program is running. When the button is pushed again, the speed goes back to what was originally set on the 
 dial and the led is turned off.
 
+
+// increased throttle is clockwise, 0 degree command
+// reduced throttle is counter-clockwise, 70 degree command
 */
 
 #include <Servo.h>          //include the servo library
 Servo myservo;              // create servo object to control a servo
 
-int potpin = 0;             // analog pin 0 used to connect the potentiometer
-int pinval = 0;             // variable to read the value from the analog pin
-int origpinval = 0;         // to store orginal pin value
+int cmdval = 35;            // variable to read the value from the analog pin  // change back to 0 after dev
+int origval = 0;            // to store orginal throttle value
+
 int FBState = 0;            // variable for reading the Full pushbutton status
 int IBState = 0;            // variable for reading the Idle pushbutton status
+int IncBState = 0;          // variable for reading the incr pushbutton status
+int DecrBState = 0;         // variable for reading the incr pushbutton status
 
 const int Fled = 4;         //Full speed button led
 const int Iled = 5;         //Idle speed button led
+const int Incled = 6;       //Incr speed button led
+const int Decrled = 7;      //Incr speed button led
+
 const int FButton = 9;      //Full speed button 
 const int IButton = 10;     //Idle speed button
+const int IncButton = 11;   //Idle speed button
+const int DecrButton = 12;  //Idle speed button
+
 const int FullPin = 65;     //Full speed setting. (93% of 70 degrees) ** Modify to change amount of throttle **
 const int IdlePin = 1;      //Idle speed setting. (1% of 70 degrees) ** Modify to change amount of throttle **
 
@@ -39,78 +50,99 @@ const int IdlePin = 1;      //Idle speed setting. (1% of 70 degrees) ** Modify t
 void setup()
 {
   myservo.attach(3);            // attaches the servo on pin 3 to the servo object
-  myservo.write(70 - pinval);   // sets the initial servo position to zero or full idle (reversed servo)
+  myservo.write(70 - cmdval);   // sets the initial servo position to zero or full idle (reversed servo)
   
   pinMode(Fled, OUTPUT);        // set the pins for the button leds to outputs
   pinMode(Iled, OUTPUT);
+  pinMode(Incled, OUTPUT);
+  pinMode(Decrled, OUTPUT);
+    
   pinMode(FButton, INPUT);      // set the pins for the buttons to inputs
   pinMode(IButton, INPUT);
+  pinMode(IncButton, INPUT);
+  pinMode(DecrButton, INPUT);
   
-  //For safety reasons, does not run the program until the dial is turned to idle. (<=2)
-  pinval = analogRead(potpin);                // reads the value of the potentiometer (value between 0 and 1023)
-  //scale it to use it with the servo (value between 0 and 70). Servos usually go from 0 to 180 degrees, this is 
-  //set to 70 degrees so it doesn't go too far and bind the throttle linkage.
-  pinval = map(pinval, 0, 1023, 70, 0);       
-    while (pinval > 2) {
-      pinval = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-      pinval = map(pinval, 0, 1023, 70, 0);   // scale it to use it with the servo (value between 0 and 70)
-    } //end while
+  //cmdval = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  //cmdval = map(cmdval, 0, 1023, 70, 0);   // scale it to use it with the servo (value between 0 and 70)
+  
 } //end setup
 
 //**************************************************************************************************************
 
 void loop()
 {
-  pinval = analogRead(potpin);              // reads the value of the potentiometer (value between 0 and 1023)
-  pinval = map(pinval, 0, 1023, 70, 0);     // scale it to use it with the servo (value between 0 and 70)
-  pinval = 70 - pinval;                     // servo direction reversed. This done so dial turns clockwise for more power
-  myservo.write(pinval);                    // sets the servo position according to the scaled value
-  delay(15);                                // waits for the servo to get there
+  IBState = digitalRead(IButton);       // read the state of the Idle pushbutton value
+  if (IBState == HIGH) {                // run the Idle speed function
+    delay(250);                         // to debounce button
+    Idle();                             // go to Idle function
+  } 
 
-  FBState = digitalRead(FButton);     // read the state of the Full pushbutton value
+  FBState = digitalRead(FButton);       // read the state of the Full pushbutton value
   if (FBState == HIGH) {            
     delay(250);
-    Full();                           // go to Full function
-    
+    Full();                             // go to Full function
   } 
-  digitalWrite(Fled, LOW);            // turn off button led
 
-  IBState = digitalRead(IButton);     // read the state of the Idle pushbutton value
+  IncBState = digitalRead(IncButton);   // read the state of the Full pushbutton value
+  if (IncBState == HIGH) {            
+    delay(100);
+    Incr();                             // go to Incr function
+  } 
+
+  DecrBState = digitalRead(DecrButton); // read the state of the Full pushbutton value
+  if (DecrBState == HIGH) {            
+    delay(100);
+    Decr();                             // go to Decr function
+  } 
   
-  if (IBState == HIGH) {              // run the Idle speed function
-    delay(250);                       // to debounce button
-    Idle();                           // go to Idle function
-  } 
-
- 
-}  // end loop
+} // end loop  
 //****************************************************************************************************
+
+void Idle() {
+  IBState = 0;                          // set button state to low after pushed
+  digitalWrite(Iled, HIGH);             // turn on button led
+  origval = cmdval;                     // store original pin value
+  while (IBState == LOW)  {             // run the Idle speed function until the Idle button is pushed again
+    cmdval = 70 - IdlePin;              // decrease speed. servo mapped from 0 to 70 degrees. This reverses servo direction.
+    myservo.write(cmdval);
+    IBState = digitalRead(IButton);     // read the state of the Idle pushbutton value
+  }
+  cmdval = origval;
+  myservo.write(cmdval);                // write the original speed to the servo
+  digitalWrite(Iled, LOW);              // turn off button led
+  delay(250);                           // to debounce button 
+}
 
 void Full() {
   digitalWrite(Fled, HIGH);             // turn on button led
-  //origpinval = pinval;                // store original pin value
+  origval = cmdval;                     // store original pin value
   while (FBState == HIGH) {
-    pinval = 70 - FullPin;              // servo mapped from 0 to 70 degrees. This reverses servo direction.
-    myservo.write(pinval);
+    cmdval = 70 - FullPin;              // servo mapped from 0 to 70 degrees. This reverses servo direction.
+    myservo.write(cmdval);
     FBState = digitalRead(FButton);     // read the state of the Full pushbutton value
   }
-  //pinval = origpinval;
-  //myservo.write(pinval);              // write the original speed to the servo
+  cmdval = origval;
+  myservo.write(cmdval);                // write the original speed to the servo
   digitalWrite(Fled, LOW);              // turn off button led
   delay(250);
 } 
 
-void Idle() {
-  IBState = 0;                         // set button state to low after pushed
-  digitalWrite(Iled, HIGH);            // turn on button led
-  //origpinval = pinval;               // store original pin value
-  while (IBState == LOW)  {            // run the Idle speed function until the Idle button is pushed again
-    pinval = 70 - IdlePin;             // decrease speed. servo mapped from 0 to 70 degrees. This reverses servo direction.
-    myservo.write(pinval);
-    IBState = digitalRead(IButton);    // read the state of the Idle pushbutton value
+void Incr() {
+  digitalWrite(Incled, HIGH);            // turn on button led
+  cmdval -= 3;                           // increase speed. servo mapped from 0 to 70 degrees. This reverses servo direction.
+  myservo.write(cmdval);
+  while (IncBState == HIGH)  {
+    IncBState = digitalRead(IncButton);  // read the state of the incr pushbutton value
   }
-  //pinval = origpinval;
-  //myservo.write(pinval);             // write the original speed to the servo
-  digitalWrite(Iled, LOW);             // turn off button led
-  delay(250);                          // to debounce button 
+  digitalWrite(Incled, LOW);             // turn off button led
+}
+
+void Decr() {
+  digitalWrite(Decrled, HIGH);            // turn on button led
+  cmdval += 3;                            // decrease speed. servo mapped from 0 to 70 degrees. This reverses servo direction.
+  myservo.write(cmdval);
+  while (DecrBState == HIGH)  {
+    DecrBState = digitalRead(DecrButton); // read the state of the decr pushbutton value
+  }
+  digitalWrite(Decrled, LOW);             // turn off button led
 }
